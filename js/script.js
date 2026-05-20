c352bfd3f4a3503460be0041f2b3759b90bfaa8e
DELL <niheleeroui124@gmail.com>
2026-05-20 15:48:25 +0200

    feat: implement signup functionality

---
+++ js/script.js
@@ -0,0 +1,119 @@
+function signup() {
+    //1. Récupération des données
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var mobile = document.getElementById("mobile").value;
+
+    //2. Validation des données
+
+    //First Name must have minimum 3 caractères
+    var isFirstNameValid = checkLength(firstName, 3);
+    if (!isFirstNameValid) {
+        showError("firstNameError", "First name must have at least 3 caracters. ");
+
+    } else {
+        clearError("firstNameError");
+    }
+    //Last Name must have minimum 4 caractères
+    var isLastNameValid = checkLength(lastName, 4);
+    if (!isLastNameValid) {
+        showError("lastNameError", "Last name must have at least 4 caracters.");
+    } else {
+        clearError("lastNameError");
+    }
+    //Format email
+    var isEmailValid = checkEmail(email)
+    if (!isEmailValid) {
+        showError("EmailError", "Invalid email format.");
+    } else {
+        clearError("EmailError");
+    }
+    //password must have min 5 caracters
+    var isPasswordValid = checkLength(password, 5);
+    if (!isPasswordValid) {
+        showError("PasswordError", "Password must have at least 5 caracters.");
+    } else {
+        clearError("PasswordError");
+    }
+    //confirm password
+    var isConfirmPasswordValid = checkPassword(confirmPassword, password);
+    if (!isConfirmPasswordValid) {
+        showError("confirmPasswordError", "Passwords do not match.");
+    } else {
+        clearError("confirmPasswordError");
+    }
+    //phone contain 8 caracters
+    var isPhone = checkPhone(mobile);
+    if (!isPhone) {
+        showError("PhoneError", "Phone must have 8 caracters.");
+    } else {
+        clearError("PhoneError");
+    }
+    // Vérifier si l'email existe déja
+    var isEmailUnique = checkEmailExists(email);
+    if (isEmailUnique) {
+        showError("EmailUniqueError", "Email must be unique");
+    } else {
+        clearError("EmailUniqueError");
+    }
+
+    if (isFirstNameValid && isLastNameValid && isEmailValid && isPasswordValid && isPhone && isConfirmPasswordValid && !isEmailUnique) {
+
+        //3. Création JSON
+        var user = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            mobile: mobile
+        }
+        console.log(user);
+
+        //4. Sauvegarde BD
+        var usersTab = JSON.parse(localStorage.getItem("users")) || [];
+
+        //ajouter un nouveau utilisateur
+        usersTab.push(user);
+
+        //4.Stockage dans local
+        localStorage.setItem("users", JSON.stringify(usersTab));
+    }
+}
+// fonction longueur chaine de caractères
+function checkLength(ch, x) {
+    return ch.length >= x;
+}
+function checkPhone(phone) {
+    return phone.length == 8;
+}
+function checkEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+//fonction pour afficher les erreurs
+function showError(id, message) {
+    document.getElementById(id).innerHTML = message;
+    document.getElementById(id).style.color = "red";
+}
+function clearError(id) {
+    document.getElementById(id).innerHTML = "";
+}
+function checkPassword(pwd, cPwd) {
+    return (pwd === cPwd);
+
+}
+function checkEmailExists(email) {
+    //récupération des utilisateurs
+    var userTab = JSON.parse(localStorage.getItem("users")) || [];
+    for (let i = 0; i < userTab.length; i++) {
+        if (userTab[i].email === email) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
